7777b4a625368852312f9559d4c2dfaf2616bfc6
HoangLong1308 <101648186+HoangLong1308@users.noreply.github.com>
2023-03-23 21:18:36 +0700

    config HttpOnlyCookie

--- src/app/service/tokenService.js
+++ src/app/service/tokenService.js
@@ -1,31 +1,29 @@
-import Cookies from 'js-cookie';
-
 const getCookieRefreshToken = () => {
-    // const refresh_token = window.localStorage.getItem('refresh_token');
-    const refresh_token = Cookies.get('refresh_token');
+    const refresh_token = window.localStorage.getItem('refresh_token');
+    // const refresh_token = Cookies.get('refresh_token');
     return refresh_token;
 };
 
 const getCookieAccessToken = () => {
-    // const access_token = window.localStorage.getItem('access_token');
-    const access_token = Cookies.get('access_token');
+    const access_token = window.localStorage.getItem('access_token');
+    // const access_token = Cookies.get('access_token');
     return access_token;
 };
 const setCookieAccessToken = (accessToken) => {
-    Cookies.set('access_token', accessToken);
-    // window.localStorage.setItem('access_token', accessToken);
+    // Cookies.set('access_token', accessToken);
+    window.localStorage.setItem('access_token', accessToken);
 };
 const setCookieRefreshToken = (refreshToken) => {
-    Cookies.set('refresh_token', refreshToken);
-    // window.localStorage.setItem('refresh_token', refreshToken);
+    // Cookies.set('refresh_token', refreshToken);
+    window.localStorage.setItem('refresh_token', refreshToken);
 };
 const removeAccessToken = () => {
-    Cookies.remove('access_token');
-    // window.localStorage.removeItem('access_token');
+    // Cookies.remove('access_token');
+    window.localStorage.removeItem('access_token');
 };
 const removeRefreshToken = () => {
-    Cookies.remove('refresh_token');
-    // window.localStorage.removeItem('refresh_token');
+    // Cookies.remove('refresh_token');
+    window.localStorage.removeItem('refresh_token');
 };
 const TokenService = {
     getCookieRefreshToken,
--- src/axios.js
+++ src/axios.js
@@ -1,110 +1,32 @@
-import { isValidToken } from 'app/contexts/JWTAuthContext';
 import axios from 'axios';
 import TokenService from 'app/service/tokenService';
-const axiosInstance = axios.create();
-
-// axiosInstance.interceptors.response.use(
-//     (response) => response,
-//     (error) =>
-//         Promise.reject(
-//             (error.response && error.response.data) || 'Axios error: error',
-//         ),
-// );
-
-// axiosInstance.interceptors.request.use(
-//     (config) => {
-//         const token = window.localStorage.getItem('access_token');
-//         if (token) {
-//             config.headers['Authorization'] = 'Bearer ' + token; // for Spring Boot back-end
-//         }
-//         return config;
-//     },
-//     (error) => {
-//         return Promise.reject(error);
-//     },
-// );
-
-axiosInstance.interceptors.request.use(
-    async (config) => {
-        const access_token = TokenService.getCookieAccessToken();
-        if (access_token) {
-            // config.headers = {
-            //     ...config.headers,
-            //     authorization: `Bearer ${access_token}`,
-            // };
-            if (isValidToken(access_token)) {
-                axiosInstance.defaults.headers.common.Authorization = `Bearer ${access_token}`;
-            } else {
-                const refresh_token = TokenService.getCookieRefreshToken();
-                const rs = await axios.post(
-                    process.env.REACT_APP_URL + 'un/refresh-token',
-                    {
-                        refresh_token: refresh_token,
-                    },
-                );
-
-                const { access_token } = rs.data;
-
-                // localStorage.setItem('access_token', access_token);
-                TokenService.setCookieAccessToken(access_token);
-                config.headers = {
-                    ...config.headers,
-                    Authorization: `Bearer ${access_token}`,
-                };
-
-                // axiosInstance.defaults.headers.common.Authorization = `Bearer ${access_token}`;
-            }
-        } else {
-            var refresh_token = TokenService.getCookieRefreshToken();
-            if (refresh_token) {
-                const rs = await axios.post(
-                    process.env.REACT_APP_URL + 'un/refresh-token',
-                    {
-                        refresh_token: refresh_token,
-                    },
-                );
-
-                const { access_token } = rs.data;
-
-                TokenService.setCookieAccessToken(access_token);
-
-                config.headers = {
-                    ...config.headers,
-                    Authorization: `Bearer ${access_token}`,
-                };
-            }
-        }
-
-        return config;
-    },
-    (error) => Promise.reject(error),
-);
+const axiosInstance = axios.create({
+    withCredentials: true,
+});
 axiosInstance.interceptors.response.use(
     (res) => {
         return res;
     },
     async (err) => {
         const originalConfig = err.config;
-        if (
-            originalConfig.url !== process.env.REACT_APP_URL + 'un/login' &&
-            err.response
-        ) {
+
+        if (originalConfig.url !== '/un/login' && err.response) {
             // Access Token was expired
-            if (err.response.status === 403 && !originalConfig._retry) {
+            console.log(!originalConfig._retry);
+            if (err.response.status === 401 && !originalConfig._retry) {
                 originalConfig._retry = true;
-                var refresh_token = TokenService.getCookieRefreshToken();
                 try {
-                    const rs = await axios.post(
-                        process.env.REACT_APP_URL + 'un/refresh-token',
-                        {
-                            refresh_token: refresh_token,
-                        },
-                    );
+                    const rs = await axiosInstance
+                        .post(process.env.REACT_APP_URL + 'un/refresh-token')
+                        .catch((error) => {
+                            window.location.reload(''); //handle when refreshtoken expired
+                        });
 
-                    const { access_token } = rs.data;
+                    if (!rs.data.access_token) {
+                        window.location.reload('');
+                    }
+                    TokenService.setCookieAccessToken(rs.data.access_token);
 
-                    TokenService.setCookieAccessToken(access_token);
-                    // axiosInstance.defaults.headers.common.Authorization = `Bearer ${access_token}`;
                     return axiosInstance(originalConfig);
                 } catch (_error) {
                     return Promise.reject(_error);
@@ -115,4 +37,5 @@ axiosInstance.interceptors.response.use(
         return Promise.reject(err);
     },
 );
+
 export default axiosInstance;
