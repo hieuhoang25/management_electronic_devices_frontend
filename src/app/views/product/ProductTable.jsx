38415b6a51b5ffb1d0eee454623e027f20e7ad56
HoangLong1308 <101648186+HoangLong1308@users.noreply.github.com>
2023-04-10 14:01:21 +0700

    Update ProductTable.jsx

--- src/app/views/product/ProductTable.jsx
+++ src/app/views/product/ProductTable.jsx
@@ -149,7 +149,6 @@ const SimpleTable = () => {
         products.stateDeleted.deleted,
         products.keysearch,
         openDialogProduct,
-        products,
     ]);
 
     function handleOpenProductAttribute(id) {
